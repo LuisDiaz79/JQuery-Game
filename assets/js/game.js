63441e47a79b88d3d0493e9cc7e55d737f5e94e5
Luis <hhavocs@yahoo.com>
2018-08-23 19:11:09 -0400

    test version

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
     $heroe.on("click", function(){
         heroeSelected = $(this).val();
         $(this).css({"display": "none"});
-        console.log("heroeSelected: " + heroeSelected);
 
         var $myheroe = $(".myheroe"); 
 
@@ -18,16 +17,11 @@ $(document).ready(function() {
         });
 
         var $enemy = $(".enemy"); 
-        console.log("ENEMY");
-        console.log($enemy);
         $enemy.each(function(index){
-            console.log($(this).attr("value"));
             if($(this).attr("value") != heroeSelected){
-                console.log(true);
                 $(this).css("display", "inline");
                 
             }else{
-                console.log(false);
                 $(this).remove();
             }
         });
@@ -38,16 +32,13 @@ $(document).ready(function() {
     $attack.on("click", function(){
         if(!gameFinished){
             var enemyAtt = $(this).attr("value");
-            console.log(enemyAtt);
             
             var enemyLife = $('#'+enemyAtt+'_life');
             var enemyPow=$('#'+enemyAtt+'_power');
-            console.log(enemyLife);
     
             var myLife =  $('#'+heroeSelected+'_life');
             var myPow = $('#'+heroeSelected+'_power');
             var myInc = $('#'+heroeSelected+'_inc');
-            console.log(myLife);
             var newLife = parseInt(myLife.text()) - parseInt(enemyPow.text());
             if(newLife <0){
                 newLife = 0;
@@ -58,13 +49,9 @@ $(document).ready(function() {
                     newEneLife =0;
                 }
                 var newpow = myInc.text().split("-");
-                console.log(newpow);
-                console.log();
                 newpow =  Math.floor(Math.random() * parseInt(newpow[1])) + parseInt(newpow[0]);
-                console.log(newpow);
                 newpow += parseInt(myPow.text())
         
-                console.log(newLife);
                 myLife.text(newLife);
                 myPow.text(newpow);
                 enemyLife.text(newEneLife);
@@ -74,7 +61,6 @@ $(document).ready(function() {
                 var totalLife = 0;
                 $enemy.each(function(index){
                     var thisEnemy = $(this).attr("value");
-                    console.log('>>enemy'+thisEnemy);
                     totalLife += parseInt($('#'+thisEnemy+'_life').text());
                 });
         
