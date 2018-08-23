cc8349ab78e6ea4eefda32d31c9fc5e8cfd75336
Luis <hhavocs@yahoo.com>
2018-08-23 19:03:30 -0400

    test version

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,36 +1,19 @@
-var char = {
-    name : "",
-    life : 0,
-    power: 0,
-    inc : 0
-}
-var enemies =[];
+var heroeSelected = "";
+var gameFinished = false;
 
 $(document).ready(function() {
     var $heroe = $(".heroe"); 
-    var heroeSelected = "";
+    
     $heroe.on("click", function(){
         heroeSelected = $(this).val();
+        $(this).css({"display": "none"});
         console.log("heroeSelected: " + heroeSelected);
 
         var $myheroe = $(".myheroe"); 
 
         $myheroe.each(function(index){
             if($(this).attr("value") != heroeSelected){
-                $(this).css({"display": "none"});
-                console.log($(this).attr("power"));
-                var tmpEnemy ={
-                    name : $(this).attr("value"),
-                    life : $(this).attr("life"),
-                    power : $(this).attr("power"),
-                    inc : $(this).attr("inc")
-                }
-                enemies.push(tmpEnemy);
-            }else{
-                char.name = $(this).attr("valnameue");
-                char.life = $(this).attr("life");
-                char.power = $(this).attr("power");
-                char.inc = $(this).attr("inc");
+                $(this).remove()
             }
         });
 
@@ -45,15 +28,74 @@ $(document).ready(function() {
                 
             }else{
                 console.log(false);
-                $(this).css({"display": "none"});
+                $(this).remove();
             }
         });
-        console.log(enemy);
     });
 
+    
     var $attack = $(".attack");
     $attack.on("click", function(){
+        if(!gameFinished){
+            var enemyAtt = $(this).attr("value");
+            console.log(enemyAtt);
+            
+            var enemyLife = $('#'+enemyAtt+'_life');
+            var enemyPow=$('#'+enemyAtt+'_power');
+            console.log(enemyLife);
+    
+            var myLife =  $('#'+heroeSelected+'_life');
+            var myPow = $('#'+heroeSelected+'_power');
+            var myInc = $('#'+heroeSelected+'_inc');
+            console.log(myLife);
+            var newLife = parseInt(myLife.text()) - parseInt(enemyPow.text());
+            if(newLife <0){
+                newLife = 0;
+            }
+            if(!parseInt(enemyLife.text())<=0){
+                var newEneLife = parseInt(enemyLife.text()) - parseInt(myPow.text());
+                if(newEneLife < 0){
+                    newEneLife =0;
+                }
+                var newpow = myInc.text().split("-");
+                console.log(newpow);
+                console.log();
+                newpow =  Math.floor(Math.random() * parseInt(newpow[1])) + parseInt(newpow[0]);
+                console.log(newpow);
+                newpow += parseInt(myPow.text())
+        
+                console.log(newLife);
+                myLife.text(newLife);
+                myPow.text(newpow);
+                enemyLife.text(newEneLife);
+        
+        
+                var $enemy = $(".enemy"); 
+                var totalLife = 0;
+                $enemy.each(function(index){
+                    var thisEnemy = $(this).attr("value");
+                    console.log('>>enemy'+thisEnemy);
+                    totalLife += parseInt($('#'+thisEnemy+'_life').text());
+                });
+        
+                if(newLife <= 0 && totalLife <= 0){
+                    myLife.text(0);
+                    enemyLife.text(0);
+                    alert('TIE');
+                }else if(totalLife <= 0 ){
+                    enemyLife.text(0);
+                    gameFinished=true;
+                    alert('WIN');
+                }else if(newLife <= 0){
+                    myLife.text(0);
+                    gameFinished=true;
+                    alert('LOSE');
+                }
+            }
+            
+        }
         
     });
+
 
 });
